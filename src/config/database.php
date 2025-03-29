5c8f8cd8e2c313e74c1dcb273de386d4fc50aefe
manueo <mail@manueo.com>
2025-03-29 18:30:07 +0100

    added commands for cli

--- src/config/database.php
+++ src/config/database.php
@@ -1,11 +1,20 @@
 <?php
+
 return [
     'mysql' => [
-        'host' => env('DANUPE_DATABASE_HOST', 'localhost'),
-        'database' => env('DANUPE_DATABASE_NAME', 'danupe'),
-        'username' => env('DANUPE_DATABASE_USERNAME', 'root'),
-        'password' => env('DANUPE_DATABASE_PASSWORD', ''),
-        'port' => env('DANUPE_DATABASE_PORT', '3306'),
-        'type' => env('DANUPE_DATABASE_CONNECTION', 'mysql')
+        'host' => danupe()->env()->get('DANUPE_DATABASE_HOST', 'localhost'),
+        'database' => danupe()->env()->get('DANUPE_DATABASE_NAME', 'danupe'),
+        'username' => danupe()->env()->get('DANUPE_DATABASE_USERNAME', 'root'),
+        'password' => danupe()->env()->get('DANUPE_DATABASE_PASSWORD', ''),
+        'port' => danupe()->env()->get('DANUPE_DATABASE_PORT', '3306'),
+        'type' => danupe()->env()->get('DANUPE_DATABASE_CONNECTION', 'mysql')
+    ],
+    'danupe' => [
+        'migrate' => ['Danupe\Plugin\Database\Classes\DatabaseManager', 'migrate', 'Migrate the database'],
+        'migrate:rollback' => ['Danupe\Plugin\Database\Classes\DatabaseManager', 'rollback', 'Rollback the last database migration'],
+        'migrate:rollback:specific' => ['Danupe\Plugin\Database\Classes\DatabaseManager', 'rollbackSpecificMigration', 'Rollback a specific database migration'],
+        'seed' => ['Danupe\Plugin\Database\Classes\DatabaseManager', 'seed', 'Seed the database'],
+        'seed:rollback' => ['Danupe\Plugin\Database\Classes\DatabaseManager', 'rollback', 'Rollback the last database seed'],
+        'seed:rollback:specific' => ['Danupe\Plugin\Database\Classes\DatabaseManager', 'rollbackSpecificSeed', 'Rollback a specific database seed'],
     ]
 ];
